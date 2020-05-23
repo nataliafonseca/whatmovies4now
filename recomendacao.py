f2b6c4b0a0414b06252fc8b232cf46789a3912ff
Natália <nataliabf@outlook.com>
2020-05-23 10:45:33 -0300

    Correção de bug quando algum dos usuários similares nunca avaliaram nenhum dos filmes com mais de 50 avaliações.

--- recomendacao.py
+++ recomendacao.py
@@ -35,7 +35,7 @@ def recomendar_para_usuario(usuario):
 
     # Então, pegamos todas as notas de todos os filmes que tem pelo
     # menos 50 avaliações
-    notas_filmes_50_ou_mais = util.get_todas_as_notas().set_index("movieId").loc[util.get_filmes_50_votos_ou_mais().index]
+    notas_dos_filmes_considerados = util.get_todas_as_notas().set_index("movieId").loc[util.get_filmes_50_votos_ou_mais().index]
 
     recomendacoes_camada1 = None
     recomendacoes_camada2 = None
@@ -44,8 +44,8 @@ def recomendar_para_usuario(usuario):
         # Resetamos o índice da DataFrame para restaurar a coluna "MovieId",
         # em seguida, setamos o índice para "userId" para buscar apenas
         # as notas dos usuários similares
-        notas_filmes_50_ou_mais = notas_filmes_50_ou_mais.reset_index()
-        notas_dos_similares = notas_filmes_50_ou_mais.set_index("userId").loc[[int(userid) for userid in camada]]
+        notas_dos_filmes_considerados = notas_dos_filmes_considerados.reset_index()
+        notas_dos_similares = notas_dos_filmes_considerados.query("userId in @camada")
         # Calculamos a média de cada filme considerando apenas as notas dos usuários similares
         media_notas_dos_similares = notas_dos_similares.groupby("movieId").mean()[["rating"]]
         # Contamos a quantidade de avaliações de cada filme pelos usuários
@@ -65,16 +65,16 @@ def recomendar_para_usuario(usuario):
         recomendacoes = recomendacoes.drop(filmes_ja_assistidos, errors="ignore")
         # Por fim, unimos à lista os titulos e demais informações dos filmes
         recomendacoes = recomendacoes.join(util.get_filmes_50_votos_ou_mais())
-        notas_filmes_50_ou_mais = notas_filmes_50_ou_mais.set_index("movieId")
+        notas_dos_filmes_considerados = notas_dos_filmes_considerados.set_index("movieId")
 
         if i == 0:
             recomendacoes_camada1 = recomendacoes.head(5)
             recomendados = recomendacoes_camada1.index
-            notas_filmes_50_ou_mais = notas_filmes_50_ou_mais.drop(recomendados, errors='ignore')
+            notas_dos_filmes_considerados = notas_dos_filmes_considerados.drop(recomendados, errors='ignore')
         elif i == 1:
             recomendacoes_camada2 = recomendacoes.head(3)
             recomendados = recomendacoes_camada2.index
-            notas_filmes_50_ou_mais = notas_filmes_50_ou_mais.drop(recomendados, errors='ignore')
+            notas_dos_filmes_considerados = notas_dos_filmes_considerados.drop(recomendados, errors='ignore')
         elif i == 2:
             recomendacoes_camada3 = recomendacoes.head(2)
 
